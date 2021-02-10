b4b4223d0fc39cb343c3056559cdbd3708d40173
carolinoreinaldo <carolinoreinaldo@gmail.com>
2021-02-10 20:56:33 -0300

    continuação do curso

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,10 +4,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 
-const routes: Routes = [{
-  path: "",
-  component: HomeComponent
-}];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "products",
+    component: ProductCrudComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
