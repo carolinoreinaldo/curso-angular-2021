0b8702b0808f207a955dd717c46953537598e448
carolinoreinaldo <carolinoreinaldo@gmail.com>
2021-02-21 19:18:51 -0300

    adição da msg de alerta e criação do primeiro service

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,9 +12,14 @@ import { NavComponent } from './components/template/nav/nav.component';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
+//MatSnackBarModule utilizado para mostrar msgs na tela
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { RedDirective } from './directives/red.directive';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +28,9 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     FooterComponent,
     NavComponent,
     HomeComponent,
-    ProductCrudComponent
+    ProductCrudComponent,
+    RedDirective,
+    ProductCreateComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +39,9 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/components/product/product-create/product.model.ts
@@ -0,0 +1,6 @@
+export interface Product {
+    // Interrogação significa que o atributo é opicional
+    id?: number
+    name: string
+    price: number
+}
